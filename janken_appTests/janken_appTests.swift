7e95de2d54ed951e5cb61e256798a46c066116b6
yamatokawai <ykawai@system-renovate.co.jp>
2025-01-24 14:40:54 +0900

    Initial Commit

---
+++ janken_appTests/janken_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  janken_appTests.swift
+//  janken_appTests
+//
+//  Created by 川井やまと on 2025/01/24.
+//
+
+import Testing
+@testable import janken_app
+
+struct janken_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ janken_appUITests/janken_appUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  janken_appUITestsLaunchTests.swift
+//  janken_appUITests
+//
+//  Created by 川井やまと on 2025/01/24.
+//
+
+import XCTest
+
+final class janken_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
