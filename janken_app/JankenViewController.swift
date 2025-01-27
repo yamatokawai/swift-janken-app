44c81225382423bc9538c7326955c8094d344fd6
yamatokawai <ykawai@system-renovate.co.jp>
2025-01-27 18:03:41 +0900

    ボタンクリックテスト

--- janken_app/JankenViewController.swift
+++ janken_app/JankenViewController.swift
@@ -3,10 +3,13 @@ import UIKit
 class JankenViewController: UIViewController {
     //タイトルラベル
     @IBOutlet weak var titleLabel: UILabel!
+    
     // 相手の手
-    @IBOutlet weak var opponentHandImg: UIImageView!
+    @IBOutlet weak var opponentHandLabel: UILabel!
+    
     // 自分の手
-    @IBOutlet weak var selfHandImg: UIImageView!
+    @IBOutlet weak var selfHandLabel: UILabel!
+    
     // グー
     @IBOutlet weak var rockButton: UIButton!
     // チョキ
@@ -14,11 +17,30 @@ class JankenViewController: UIViewController {
     // パー
     @IBOutlet weak var paperButton: UIButton!
     
+    
+    let titleText = "じゃんけん"
+    let hands = ["👊","✌️","✋"]
+//    let opponentHand: String = hands.randomElement()
+    let rockLabel = "グー👊"
+    let scissorsLabel = "チョキ✌️"
+    let paperLabel = "パー✋"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("aaa")
+        generateView()
     }
 
-
+    func generateView() {
+        titleLabel.text = titleText
+        rockButton.setTitle(rockLabel, for:UIControl.State.normal)
+        scissorsButton.setTitle(scissorsLabel, for:UIControl.State.normal)
+        paperButton.setTitle(paperLabel, for:UIControl.State.normal)
+    }
+    
+    @IBAction func clickRockButton(_ sender: Any) {
+        opponentHandLabel.text = hands.randomElement()
+    }
+    
 }
 
