31f087b7bb98513a12a92a984cf51a501996a960
yamatokawai <ykawai@system-renovate.co.jp>
2025-01-28 17:34:10 +0900

    ビューある程度作成

--- janken_app/JankenViewController.swift
+++ janken_app/JankenViewController.swift
@@ -3,7 +3,8 @@ import UIKit
 class JankenViewController: UIViewController {
     //タイトルラベル
     @IBOutlet weak var titleLabel: UILabel!
-    
+    // スタートボタン
+    @IBOutlet weak var startButton: UIButton!
     // 相手の手
     @IBOutlet weak var opponentHandLabel: UILabel!
     
@@ -19,25 +20,46 @@ class JankenViewController: UIViewController {
     
     
     let titleText = "じゃんけん"
+    let opponentLabel = "？"
+    let startBtnLabel = "スタート"
+    let endBtnLabel = "終了"
     let hands = ["👊","✌️","✋"]
+    var elemsDisp: [UIView] = []
     let rockLabel = "グー👊"
     let scissorsLabel = "チョキ✌️"
     let paperLabel = "パー✋"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        elemsDisp = [selfHandLabel,rockButton,scissorsButton,paperButton]
         generateView()
     }
 
     func generateView() {
         titleLabel.text = titleText
+        startButton.setTitle(startBtnLabel, for:UIControl.State.normal)
+        opponentHandLabel.text = opponentLabel
         rockButton.setTitle(rockLabel, for:UIControl.State.normal)
         scissorsButton.setTitle(scissorsLabel, for:UIControl.State.normal)
         paperButton.setTitle(paperLabel, for:UIControl.State.normal)
+        for elem in elemsDisp {
+            elem.isHidden = true
+        }
     }
     
-    @IBAction func clickRockButton(_ sender: Any) {
-        opponentHandLabel.text = hands.randomElement()
+    @IBAction func clickStartButton(_ sender: Any) {
+        if startButton.currentTitle == startBtnLabel {
+            for elem in elemsDisp {
+                elem.isHidden = false
+            }
+            startButton.setTitle(endBtnLabel, for:UIControl.State.normal)
+        }else{
+            for elem in elemsDisp {
+                elem.isHidden = true
+            }
+            startButton.setTitle(startBtnLabel, for:UIControl.State.normal)
+        }
+        
     }
     
 }
