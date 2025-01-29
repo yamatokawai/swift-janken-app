40138dc3be151c28166c361bb7deb4b090569e1f
yamatokawai <ykawai@system-renovate.co.jp>
2025-01-29 14:17:03 +0900

    一旦完成

--- janken_app/JankenViewController.swift
+++ janken_app/JankenViewController.swift
@@ -18,31 +18,34 @@ class JankenViewController: UIViewController {
     // パー
     @IBOutlet weak var paperButton: UIButton!
     
+    @IBOutlet weak var resultLabel: UILabel!
     
     let titleText = "じゃんけん"
-    let opponentLabel = "？"
+    let labelOrigin = "？"
     let startBtnLabel = "スタート"
     let endBtnLabel = "終了"
     let hands = ["👊","✌️","✋"]
     var elemsDisp: [UIView] = []
-    let rockLabel = "グー👊"
-    let scissorsLabel = "チョキ✌️"
-    let paperLabel = "パー✋"
+    let rockLabel = "👊"
+    let scissorsLabel = "✌️"
+    let paperLabel = "✋"
     var titleOriginalPosition: CGPoint!
     var startOriginalPosition: CGPoint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        elemsDisp = [opponentHandLabel, selfHandLabel,rockButton,scissorsButton,paperButton]
+        elemsDisp = [opponentHandLabel, selfHandLabel,resultLabel,rockButton,scissorsButton,paperButton]
         titleOriginalPosition = titleLabel.frame.origin
         startOriginalPosition = startButton.frame.origin
         generateView()
     }
 
+    
     func generateView() {
         titleLabel.text = titleText
         startButton.setTitle(startBtnLabel, for:UIControl.State.normal)
-        opponentHandLabel.text = opponentLabel
+        opponentHandLabel.text = labelOrigin
+        selfHandLabel.text = labelOrigin
         rockButton.setTitle(rockLabel, for:UIControl.State.normal)
         scissorsButton.setTitle(scissorsLabel, for:UIControl.State.normal)
         paperButton.setTitle(paperLabel, for:UIControl.State.normal)
@@ -52,18 +55,17 @@ class JankenViewController: UIViewController {
         }
     }
     
-    func getRandomHand() {
-        opponentHandLabel.text = hands.randomElement()
-    }
-    
     @IBAction func clickStartButton(_ sender: Any) {
         if startButton.currentTitle == startBtnLabel {
             for elem in elemsDisp {
                 elem.isHidden = false
             }
+            resultLabel.text = ""
+            opponentHandLabel.text = labelOrigin
+            selfHandLabel.text = labelOrigin
             startButton.setTitle(endBtnLabel, for:UIControl.State.normal)
             UIView.animate(withDuration: 0.3) {
-                        self.startButton.frame.origin = CGPoint(x: 50, y: 100)
+                        self.startButton.frame.origin = CGPoint(x: 0, y: 100)
                     }
         }else{
             for elem in elemsDisp {
@@ -75,8 +77,22 @@ class JankenViewController: UIViewController {
             }
         }
     }
-    @IBAction func selectSelfHand(_ sender: Any) {
-        getRandomHand()
+    
+    @IBAction func cllickSelfHands(_ sender: Any) {
+        opponentHandLabel.text = hands.randomElement()
+        
+        if let button = sender as? UIButton {
+            selfHandLabel.text = button.currentTitle
+            switch (selfHandLabel.text, opponentHandLabel.text ) {
+            case ( rockLabel, scissorsLabel), (scissorsLabel, paperLabel), (paperLabel, rockLabel):
+                resultLabel.text = "あなたの勝ち!"
+            case (scissorsLabel, rockLabel), (paperLabel, scissorsLabel), (rockLabel, paperLabel):
+                resultLabel.text = "あなたの負け!"
+            default:
+                resultLabel.text = "引き分け!"
+            }
+        }
+        
     }
     
 }
